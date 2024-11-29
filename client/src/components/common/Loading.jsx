b260d29579027194d45001cabaabcaf851d2e0f1
pankajsharma81 <pankajsharma88279@gmail.com>
2024-11-29 14:33:39 +0530

    add Stack on Loading

--- client/src/components/common/Loading.jsx
+++ client/src/components/common/Loading.jsx
@@ -1,7 +1,20 @@
-const Loading = () =>{
+// eslint-disable-next-line no-unused-vars
+import { Stack, CircularProgress } from "@mui/material";
+
+const Loading = () => {
   return (
-    <h1>Loading...</h1>
-  )
-}
+    <Stack
+      flexDirection={"row"}
+      minHeight={"50vh"}
+      width={"100%"}
+      height={"100%"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      my={5}
+    >
+      loader Stack
+    </Stack>
+  );
+};
 
 export default Loading;
