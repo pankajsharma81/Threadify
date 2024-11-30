1e2cd05f795772b09e74a59c600f5452f18b0e17
pankajsharma81 <pankajsharma88279@gmail.com>
2024-11-30 18:18:57 +0530

    changes in App.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
-import Loading from "./components/common/Loading";
+// import Loading from "./components/common/Loading";
+import Header from "./components/common/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <Loading />
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<h1>Home</h1>} />
+          <Route exact path="/contact" element={<h1>Contact</h1>} />
+          <Route exact path="/user" element={<h1>User profile</h1>} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
